c833daa0ec92c1de9bb881279cfb76e11d4a56b7
sethbunke <sethbunke@hotmail.com>
2017-12-09 00:33:00 -0800

    very basic model working

---
+++ model.py
@@ -0,0 +1,39 @@
+import csv
+import cv2
+import numpy as np
+
+from keras.models import Sequential
+from keras.layers import Flatten, Dense
+
+lines = []
+
+
+
+with open('../simulator/data/driving_log.csv') as csvfile:
+    reader = csv.reader(csvfile)
+    next(reader)
+    for line in reader:
+        lines.append(line)
+
+images = []
+measurements = []
+for line in lines:
+    source_path = line[0]
+    filename = source_path.split('/')[-1]
+    current_path = '../simulator/data/IMG/' + filename
+    image = cv2.imread(current_path)
+    images.append(image)
+    measurement = float(line[3])
+    measurements.append(measurement)
+
+X_train = np.array(images)
+y_train = np.array(measurements)
+
+model = Sequential()
+model.add(Flatten(input_shape=(160,320,3)))
+model.add(Dense(1))
+
+model.compile(loss='mse', optimizer='adam')
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=7)
+
+model.save('tes_model.h5')
